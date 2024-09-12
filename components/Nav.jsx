779e77e88668783fb1c21d377c2f72caafbe77c2
Shreyansh Kumar <shreyanshkumar1403@gmail.com>
2024-09-12 10:51:03 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -19,7 +19,7 @@ const Nav = () => {
   
   return (
     <nav className='w-full fixed px-2 py-4 border-b bg-white z-50'>
-      <div className='flex items-center justify-between px-4 w-2/3 mx-auto'>
+      <div className='flex items-center justify-between px-4 w-full md:w-2/3 mx-auto'>
       <Link href={'/'} className='font-bold text-xl outline-none'>ShreyanshKr.</Link>
       <div className='flex items-center justify-end lg:justify-between w-fit md:w-1/3 lg:w-1/3 gap-2 lg:gap-0 cursor-pointer font-medium outline-none '>
         <Link href={'/'} className='hidden md:inline'>Home</Link>
